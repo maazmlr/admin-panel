d0900f94538432bbd8d54bc42c6e5f8d00ccb6f3
muhammad maaz <compiletimeterror@gmail.com>
2024-02-10 14:00:41 +0500

    commmit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen bg-black w-20'>Home</div>
+    <div className=' h-[89.6vh] bg-black w-20'>Home</div>
   )
 }
 
